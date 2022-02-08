0da3c548ab3437d1528cf7d26ea0036c06fae5df
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2022-02-08 17:34:38 +0200

    Add web parser library

---
+++ WebParser/UrlParser.fs
@@ -0,0 +1,30 @@
+﻿namespace HtmlParser
+
+open System
+open System.Net.Http
+
+module UrlParser =
+    let isAbsoluteUrl(url: string) = url.Contains("http")
+    
+    let getUrlSheme(url: string) = 
+        match isAbsoluteUrl url with
+        | true -> 
+            url.Split([|"://"|], StringSplitOptions.TrimEntries).[0]
+        | false -> ""
+
+    let getSubdomain(url: string) =
+        let countChars(str: string, c: char) = str |> Seq.filter (fun x -> x = c) |> Seq.length
+        
+        match isAbsoluteUrl url with
+        | true ->
+            let urlWithoutSchema = url.Split([|"://"|], StringSplitOptions.TrimEntries).[1]
+            if countChars(urlWithoutSchema, '.') >= 2 then urlWithoutSchema.Split('.').[0] else ""
+        | false -> ""
+
+    let getUrlHost url =
+        match isAbsoluteUrl url with
+        | true ->
+            let subdomain = getSubdomain url
+            let urlWithoutSchema = url.Split([|"://"|], StringSplitOptions.TrimEntries).[1]
+            urlWithoutSchema.Replace(subdomain, "").Split('/').[0]
+        | false -> ""
